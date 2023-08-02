c6375c70b8703f750458edfffb5d186f1780214f
Jyd Rey Mercado <92672461+Jeydori@users.noreply.github.com>
2023-08-03 04:37:02 +0800

    source code

--- GPS and SMS Heart Monitoring Device.ino
+++ GPS and SMS Heart Monitoring Device.ino
@@ -6,7 +6,7 @@
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_DEV_0 | U8G_I2C_OPT_NO_ACK | U8G_I2C_OPT_FAST); 
 char str[10];
 
-const byte button = 2;
+const byte button = 7;
 byte buttState;
 
 SoftwareSerial gpsSerial(8, 9); 
